7075c95a5bca6981bba27115c52b8922baa973e9
Bilal Arslan <bilalarslan11@gmail.com>
2025-09-07 15:18:43 +0200

    base urls changed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            { source: "/games", destination: "/api/games" },
+            { source: "/games/search", destination: "/api/games/search" },
+            { source: "/games/top-rated", destination: "/api/games/top-rated" },
+            { source: "/games/:id", destination: "/api/games/:id" },
+        ];
+    },
 };
 
 export default nextConfig;
