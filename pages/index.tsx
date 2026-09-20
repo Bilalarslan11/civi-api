c874aba16283fb7500fb6d1a1bd36b4b951776ca
Bilal Arslan <bilalarslan11@gmail.com>
2026-09-20 15:04:36 +0200

    hw2

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <div>Hello world!</div>
+        <div>Hello world2!</div>
       </main>
     </>
   );
